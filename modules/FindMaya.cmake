856f305b85ae06a93f98369bea8e5529f1a3c619
kingmax <kingmax_res@163.com>
2019-09-10 13:44:23 +0800

    * -dMAYA_VERSION=2018

--- modules/FindMaya.cmake
+++ modules/FindMaya.cmake
@@ -44,7 +44,7 @@
 
 # Set a default Maya version if not specified
 if(NOT DEFINED MAYA_VERSION)
-    set(MAYA_VERSION 2017 CACHE STRING "Maya version")
+    set(MAYA_VERSION 2018 CACHE STRING "Maya version")
 endif()
 
 # OS Specific environment setup
